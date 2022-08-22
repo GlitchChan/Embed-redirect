89271bba0cb75f578f17564ad1212d6af09b2281
Glitchy <thepatheticweebgamer@gmail.com>
2022-08-22 17:06:06 -0400

    Fix settings?

--- Settings.jsx
+++ Settings.jsx
@@ -1,7 +1,5 @@
 import { setDefaults, dependPersist, SSwitch, SSelect } from "cumcord-tools";
 
-let invidiousInstances = fetch("https://api.invidious.io/instances.json").then(data => data.json())
-
 setDefaults({
     "enableInvidious": true,
     "invidiousInstance": "https://yewtu.be",
@@ -16,17 +14,6 @@ export default dependPersist(() => (
         <SSelect k="changeExistingMessages">Change links existing embeds</SSelect>
         <SSwitch k="changeSentMessages">Change links of sent messages</SSwitch>
         <SSwitch k="enableInvidious">Enable Invidious redirects</SSwitch>
-        <SSelect
-            k="invidiousInstance"
-            depends="enableInvidious"
-            options={[
-                invidiousInstances.map(s => {
-                    return {value: `https://${s[0]}`, label: s[0]}
-                })
-            ]}
-        >
-            Invidious Instance
-        </SSelect>
         <SSwitch k="enableTwit">Enable Twitter redirects</SSwitch>
         <SSelect
             k="fxOrVx"
